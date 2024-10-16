2308c76a0e7e200c0f3b00c1149e883fa3727b52
Balan-HK <47158607+Balan-HK@users.noreply.github.com>
2024-10-17 00:40:31 +0200

    Update bot.js

--- src/modules/discord/bot.js
+++ src/modules/discord/bot.js
@@ -4,9 +4,13 @@ const roleHandlers = require("./roleHandlers");
 const eventManager = require("../../lib/eventManager");
 
 // Initialize the Discord client with necessary intents
-// const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMemberManager] });
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
-const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers, // Correct intent to fetch guild members
+  ],
+});
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 /**
  * Event listener for when the bot is ready.
@@ -37,11 +41,20 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
+/**
+ * Registers application commands when the bot is ready.
+ */
 eventManager.on("discord:bot:ready", async () => {
-  await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.DISCORD_GUILD_ID), { body: await commandHandlers.getCommands() });
-  console.log("Successfully registered application commands.");
+  try {
+    const commands = await commandHandlers.getCommands();
+    await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.DISCORD_GUILD_ID), { body: commands });
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error("Error registering application commands:", error);
+  }
 });
 
+// Export the login function for starting the bot
 exports.login = async function () {
   await client.login(process.env.DISCORD_TOKEN);
 };
@@ -49,6 +62,5 @@ exports.login = async function () {
 // Export updateDiscordUserRoles function if needed elsewhere
 exports.updateDiscordUserRoles = async function (users) {
   console.log("updateDiscordUserRoles");
-  // Logic to update Discord user roles based on provided data
   await roleHandlers.updateRoles(client, users);
 };
